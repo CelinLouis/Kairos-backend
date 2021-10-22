fefe78bb329ca99a2fff1065a9f1d39dcfce1545
HashM <rayanstive9@gmail.com>
2021-10-22 09:19:33 +0300

    Setting django

--- back/settings.py
+++ back/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
